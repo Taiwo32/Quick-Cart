37490e4a021fe2d00bc1a9fbe3b90b76e05e5863
Taiwo32 <taiwosolution02@gmail.com>
2025-08-05 14:13:07 +0100

    Fix typo in inngest

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,8 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/config/inngest";
+// import { inngest } from "@/config/inngest";
 import { syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inggest";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
+
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
